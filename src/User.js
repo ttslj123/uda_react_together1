5ec7cfd62cb1799ef7e47cc33176b2e9f3362b12
ttslj111 <ttslj111@gmail.com>
2020-08-11 18:45:09 +0000

    first commit

---
+++ src/User.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+const User = props => {
+  //return JSX
+  return (
+    <li className="user">
+      <p>Username: {props.user.username}</p>
+      <p>Number of Games Played: {props.showGamesPlayed ? props.user.numGamesPlayed : '*'}</p>
+    </li>
+  );
+};
+
+User.propTypes = {
+  showGamesPlayed: PropTypes.bool.isRequired,
+  user: PropTypes.object.isRequired,
+};
+
+export default User;
